be7a01f26224294d9d4f50614ce444bf0dcadbd9
Andrew E. Slaughter <andrew.e.slaughter@gmail.com>
2012-06-14 01:16:11 -0400

    Changed installer; in process of updating build process; updated documentation to refer to new website and Ubuntu One

--- YCbuild.m
+++ YCbuild.m
@@ -1,30 +1,20 @@
-function YCbuild(action,varargin)
+function YCbuild(varargin)
 % YCBUILD compiles and posts the YCweather software package
 %__________________________________________________________________________
-% SYNTAX: YCbuild('web')
-%         YCbuild('test')
-%         YCbuild('build')
+% SYNTAX: YCbuild(ver)
 %
 % DESCRIPTION:
-%       YCbuild('web') copies existing copy of YCsetup to caesar
-%       YCbuild('test') copies existing copy of YCsetup contents to desktop
-%       YCbuiid('build') compiles a new version of YCsetup with mcc
+%       YCbuild compiles a new version of setup.exe, asking for version
+%       YCbuiid(ver) compiles a new version of with given version num.
 %
 % PROGRAM OUTLINE:
 % 1 - SET FOLDER AND FILE NAMES
 % 2 - RUN MATLAB COMPILIER TO BUILD A NEW RELEASE APPLICATION
-% 3 - POST LATEST VERSION TO THE WEB
-% 4 - POST FILES TO DESKTOP
 %__________________________________________________________________________
 
 % 1 - SET FOLDER AND FILE NAMES
-    % 1.1 - Compiled release application folders
-        prog = [cd,'\'];
-        src = [cd,'\YCmain\src\']; mkdir(src);
-        pkg = [cd,'\YCinstaller\src\']; mkdir(pkg);
-        real = [cd,'\release\'];
-        serv = '/home/cesubzero/html/snow/';
-        jammer = 'C:\Program Files (x86)\InstallJammer\';
+    % 1.1 - Command for compiling installer
+        iscc = 'C:\Program Files (x86)\Inno Setup 5\compil32 /cc "installer_script.iss"';
 
     % 1.2 - Specific files to package into YCmain
         files{1} = [src,'YCmain.exe'];
@@ -34,18 +24,11 @@ function YCbuild(action,varargin)
         files{5} = [real,'winscp.exe'];
         files{6} = [real,'license.txt'];
 
-    % 1.3 - Specify the installer package
-        installer = [prog,'YCinstallerFiles\',...
-                    'output\YCweather-0.9-Setup.exe'];
-                
-    % 1.4 - Create new version of help.pdf
-        latex = [prog,'documentation\main_YCweather.pdf'];
+    % 1.3 - Create new version of help.pdf
+        latex = 'documentation\main_YCweather.pdf';
         copyfile(latex,files{3},'f');
   
 % 2 - RUN MATLAB COMPILIER TO BUILD A NEW RELEASE APPLICATION
-switch action
-	case 'build';
-        
     % 2.1 - Copy external programs into current directory
         cfile = {'XYscatter\XYscatter.m','GetUnit\getunit.m',...
                 'GetUnit\units.txt','waitbar.m','icons.ico'};
@@ -55,8 +38,8 @@ switch action
         end
 
     % 2.2 - Run compilers on YCmain and YCinstaller
-        m = 'mcc -F YCmain.prj'; eval(m);
-        m = 'mcc -F YCinstaller.prj'; eval(m);
+        m = 'deploytool -build YCmain.prj'; eval(m);
+        m = 'deploytool -build YCinstaller.prj'; eval(m);
 
     % 2.4 - Copy format *.yc and database files to the release folder
         copyDBfiles(real);
@@ -81,53 +64,34 @@ switch action
         copyfile([pkg,'YCweather.exe'],real,'f');
         copyfile([real,'version.txt'],prog,'f');
 
-    % 2.7 - Create/copy Windows installer program
-        cd(jammer);
-        install_pkg = [prog,'YCinstallerFiles\YCinstaller.mpi'];
-        jam = ['!installjammer --build ',install_pkg];
-        eval(jam);
-        copyfile(installer,[real,'YCinstaller.exe']);
-        cd(prog);
+    % 2.7 - Create Windows installer program
+        eval(iscc);
         
 % 3 - POST LATEST VERSION TO THE WEB/DESKTOP
-    case 'web';   
-    % 3.1 - Establish generic put/rm commands
-        A{1} = '!winscp.exe ';
-        A{2} = 'snow:Sno$2008@caesar.ce.montana.edu /command ';
-        A{4} = ' "exit"'; 
-        post = {'YCmain.zip','version.txt','help.pdf','YCinstaller.exe'};    
-
-    % 3.2 - Update files
-        for i = 1:length(post);
-            disp(['Removing old file... ',post{i}]);
-            A{3} = ['"rm ',serv,post{i},'"']; eval([A{:}]);
-            disp(['Uploading new file... ',post{i}]);
-            A{3} = ['"put ',real,post{i},' ',serv,'"']; eval([A{:}]);  
-        end
-
-    % 3.3 - Sync format folder
-        current = getfolder;
-        A{3} = ['"synchronize remote "',real,'database\',current,...
-            '\*.yc" /home/snow/db/" '];
-        A{4} = '"rmdir /home/snow/db/.svn/" ';
-        A{5} = '"exit"'; 
-        disp('Synchronizing release\format folder...');
-        eval([A{:}]);
-        disp('Web posting complete.');
-
-% 4 - POST FILES TO DESKTOP
-    case 'test';
-        loc = 'C:\Users\pigpen\Desktop\YCweather'; mkdir(loc);
-        copyfile(files{1},loc,'f'); 
-        copyfile(files{2},loc,'f');
-        copyfile([pkg,'YCweather.exe'],loc,'f');
-
-% 5 - COMPLETE TEST
-    case 'install'
-        loc = 'C:\Users\pigpen\Desktop';
-        file = [real,'YCinstaller.exe'];
-        copyfile(file,loc,'f');
-end
+%     case 'web';   
+%     % 3.1 - Establish generic put/rm commands
+%         A{1} = '!winscp.exe ';
+%         A{2} = 'snow:Sno$2008@caesar.ce.montana.edu /command ';
+%         A{4} = ' "exit"'; 
+%         post = {'YCmain.zip','version.txt','help.pdf','YCinstaller.exe'};    
+% 
+%     % 3.2 - Update files
+%         for i = 1:length(post);
+%             disp(['Removing old file... ',post{i}]);
+%             A{3} = ['"rm ',serv,post{i},'"']; eval([A{:}]);
+%             disp(['Uploading new file... ',post{i}]);
+%             A{3} = ['"put ',real,post{i},' ',serv,'"']; eval([A{:}]);  
+%         end
+% 
+%     % 3.3 - Sync format folder
+%         current = getfolder;
+%         A{3} = ['"synchronize remote "',real,'database\',current,...
+%             '\*.yc" /home/snow/db/" '];
+%         A{4} = '"rmdir /home/snow/db/.svn/" ';
+%         A{5} = '"exit"'; 
+%         disp('Synchronizing release\format folder...');
+%         eval([A{:}]);
+%         disp('Web posting complete.');
 
 %--------------------------------------------------------------------------
 % SUBFUNCTION: copyDBfiles
@@ -144,7 +108,6 @@ function copyDBfiles(instl)
     yc = dir([loc,'*.yc']);
     meso = [loc,'mesowest.txt'];
     
-
     if ~isempty(dat); 
         for i = 1:length(dat); 
             copyfile([loc,dat(i).name],out,'f'); 
